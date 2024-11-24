234aebbb9f252fd892bd7e257119e6fdf92c4780
kev <jankevin.dionaldo64@gmail.com>
2024-11-25 00:35:35 +0800

    Feat: Added logo; fixed forms and organized folders/path

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,28 +1,74 @@
+<!--Programming naming conventions-->
+
 <script setup>
 import AppLayout from '@/components/layout/AppLayout.vue'
 import { useDisplay } from 'vuetify'
-import LoginForm from '@/components/auth/LoginForm.vue'
+import '@/assets/login_style.css'
 
 const { mobile } = useDisplay()
+
+import { ref } from 'vue'
+
+const visible = ref(false)
 </script>
 
-<template>
+<!--/public/images/IMG_20241124_194845_692.PNG-->
+<template id="background">
   <AppLayout>
     <template #content>
       <v-row class="">
-        <v-col cols="12" md="6" class="mx-auto">
-          <v-card class="mx-auto" elevation="24">
+        <v-col cols="12" md="6" class="mx-auto" style="margin-top: 3%">
+          <v-card elevation="24" class="mx-auto" width="100%">
             <v-card-title class="text-center">
-              <v-img class="mx-auto" src="/img/logo.png" :width="mobile ? '75%' : '25%'"></v-img>
-              <h3 class="font-weight-black text-center">Welcome to Cam'n Go</h3>
-              <p>Login Form</p>
+              <v-img
+                src="/public/images/logo.PNG"
+                alt="Logo"
+                :width="mobile ? '75%' : '45%'"
+                class="d-block mx-auto"
+              ></v-img>
+              <p id="cam_font" class="text-center">CAM'N GO</p>
+              <small>Login</small>
             </v-card-title>
 
-            <v-card-text class="bg-surface-light pt-4">
-              <v-divider class="my-5"></v-divider>
-              <LoginForm> </LoginForm>
-              <v-divider class="my-5"></v-divider>
-            </v-card-text>
+            <v-sheet class="mx-auto" width="85%">
+              <v-form fast-fail @submit.prevent>
+                <v-text-field
+                  clearable
+                  label="User Name"
+                  placeholder="Enter your User Name"
+                  persistent-clear
+                  prepend-inner-icon="mdi-email-outline"
+                ></v-text-field>
+
+                <v-text-field
+                  label="Password"
+                  :append-inner-icon="visible ? 'mdi-eye' : 'mdi-eye-off'"
+                  :type="visible ? 'text' : 'password'"
+                  placeholder="Enter your password"
+                  prepend-inner-icon="mdi-lock-outline"
+                  @click:append-inner="visible = !visible"
+                ></v-text-field>
+
+                <v-class class="d-flex justify-space-between">
+                  <label class="checkbox-container">
+                    <input class="custom-checkbox" type="checkbox" />
+                    <span class="checkmark"></span>
+                    <small>Remember Me</small>
+                  </label>
+                  <h6 class="text-right">Forgot Password?</h6>
+                </v-class>
+
+                <v-btn class="mt-2" type="Login" prepend-icon="mdi-login" block>Login</v-btn>
+              </v-form>
+
+              <v-divider class="my-4"></v-divider>
+              <h6 class="my-3 text-center">
+                Don't have an account?
+                <RouterLink to="/register" class="text-primary" style="text-decoration: none"
+                  >Sign up here<v-icon icon="mdi-chevron-right"></v-icon
+                ></RouterLink>
+              </h6>
+            </v-sheet>
           </v-card>
         </v-col>
       </v-row>
--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,31 +1,132 @@
 <script setup>
 import AppLayout from '@/components/layout/AppLayout.vue'
-import { useDisplay } from 'vuetify'
-import RegisterForm from '@/components/auth/RegisterForm.vue'
+import '@/assets/login_style.css'
 
-const { mobile } = useDisplay()
+//For pass verification
+import { ref } from 'vue'
+
+const rules = {
+  required: (value) => !!value || 'Required.',
+  min: (v) => v.length >= 8 || 'Min 8 characters',
+  emailMatch: () => `The email and password you entered don't match`,
+}
+
+const show1 = ref(false)
+const show2 = ref(true)
+const password = ref('Password')
+
+//for eye
+
+const visible = ref(false)
 </script>
 
 <template>
   <AppLayout>
     <template #content>
-      <v-row class="">
+      <v-row>
         <v-col cols="12" md="6" class="mx-auto">
-          <v-card class="mx-auto" elevation="24">
+          <v-card class="mx-auto" width="100%" elevation="24" style="margin-top: 3%">
             <v-card-title class="text-center">
-              <v-img class="mx-auto" src="/img/logo.png" :width="mobile ? '75%' : '25%'"></v-img>
-              <h3 class="font-weight-black text-center">Welcome to Cam'n Go</h3>
-              <p>Register Form</p>
+              <v-img
+                src="/public/images/logo.PNG"
+                alt="Logo"
+                :width="mobile ? '75%' : '45%'"
+                class="d-block mx-auto"
+              ></v-img>
+              <p id="cam_font" class="text-center">CAM'N GO</p>
+              <small>Register Form</small>
             </v-card-title>
 
-            <v-card-text class="bg-surface-light pt-4">
-              <v-divider class="my-5"></v-divider>
-              <RegisterForm> </RegisterForm>
-              <v-divider class="my-5"></v-divider>
-            </v-card-text>
+            <v-sheet class="mx-auto" width="85%">
+              <v-form fast-fail @submit.prevent>
+                <v-row>
+                  <v-col cols="12" md="6">
+                    <v-text-field
+                      clearable
+                      label="First Name"
+                      placeholder="Your First Name"
+                      persistent-clear
+                    ></v-text-field>
+                  </v-col>
+
+                  <v-col cols="12" md="6">
+                    <v-text-field
+                      clearable
+                      label="Last Name"
+                      placeholder="Your Last Name"
+                      persistent-clear
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+
+                <v-text-field
+                  clearable
+                  v-model="email"
+                  :rules="emailRules"
+                  label="E-mail"
+                  required
+                ></v-text-field>
+
+                <v-text-field
+                  clearable
+                  label="User Name"
+                  placeholder="Enter your User Name"
+                  persistent-clear
+                  :rules="[rules.required, rules.min]"
+                  hint="At least 8 characters"
+                  name="input-10-1"
+                  counter
+                  @click:append="show1 = !show1"
+                ></v-text-field>
+
+                <v-text-field
+                  label="Password"
+                  :append-inner-icon="visible ? 'mdi-eye' : 'mdi-eye-off'"
+                  :type="visible ? 'text' : 'password'"
+                  placeholder="Enter your password"
+                  @click:append-inner="visible = !visible"
+                  :rules="[rules.required, rules.min]"
+                  hint="At least 8 characters"
+                  name="input-10-1"
+                  counter
+                  @click:append="show1 = !show1"
+                ></v-text-field>
+
+                <v-btn class="my-4" type="Register" append-icon="mdi-account-plus-outline" block
+                  >Register</v-btn
+                >
+
+                <h6 class="my-3 text-center">
+                  Already have an account?
+                  <RouterLink to="/" class="text-primary" style="text-decoration: none"
+                    >Back to Login<v-icon icon="mdi-chevron-right"></v-icon
+                  ></RouterLink>
+                </h6>
+              </v-form>
+            </v-sheet>
           </v-card>
         </v-col>
       </v-row>
     </template>
   </AppLayout>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    email: '',
+    emailRules: [
+      (value) => {
+        if (value) return true
+
+        return 'E-mail is required.'
+      },
+      (value) => {
+        if (/.+@.+\..+/.test(value)) return true
+
+        return 'E-mail must be valid.'
+      },
+    ],
+  }),
+}
+</script>
