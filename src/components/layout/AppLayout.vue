8024978d9740842553ecd9d102cf8a11b337b851
kev <jankevin.dionaldo64@gmail.com>
2024-11-30 18:09:09 +0800

    applayout

--- src/components/layout/AppLayout.vue
+++ src/components/layout/AppLayout.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { ref } from 'vue'
-const theme = ref('dark')
+const theme = ref(localStorage.getItem('theme') ?? 'dark')
 
 function onClick() {
   theme.value = theme.value === 'dark' ? 'light' : 'dark'
+  localStorage.setItem('theme', theme.value)
 }
 </script>
 
