f5f8abd0f326a6a1dc14131e4bb9e757ba6c6c5e
kev <jankevin.dionaldo64@gmail.com>
2024-11-30 13:45:28 +0800

    feat: added validators for loginform; Fix: organized folders and added utilities folders

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -17,33 +17,35 @@ const visible = ref(false)
 <template id="background">
   <AppLayout>
     <template #content>
-      <v-row class="">
-        <v-col cols="12" md="6" class="mx-auto" style="margin-top: 3%">
-          <v-card elevation="24" class="mx-auto" width="100%">
-            <v-card-title class="text-center">
-              <v-img
-                src="/images/logo.png"
-                alt="Logo"
-                :width="mobile ? '75%' : '45%'"
-                class="d-block mx-auto"
-              ></v-img>
-              <p id="cam_font" class="text-center">CAM'N GO</p>
-              <small>Login</small>
-            </v-card-title>
+      <v-container>
+        <v-row class="">
+          <v-col cols="12" md="6" class="mx-auto" style="margin-top: 3%">
+            <v-card elevation="24" class="mx-auto" width="100%">
+              <v-card-title class="text-center">
+                <v-img
+                  src="/images/logo.png"
+                  alt="Logo"
+                  :width="mobile ? '75%' : '45%'"
+                  class="d-block mx-auto"
+                ></v-img>
+                <p id="cam_font" class="text-center">CAM'N GO</p>
+                <small>Login</small>
+              </v-card-title>
 
-            <v-sheet class="mx-auto" width="85%">
-              <LoginForm> </LoginForm>
-              <v-divider class="my-4"></v-divider>
-              <h6 class="my-3 text-center">
-                Don't have an account?
-                <RouterLink to="/register" class="text-primary" style="text-decoration: none"
-                  >Sign up here<v-icon icon="mdi-chevron-right"></v-icon
-                ></RouterLink>
-              </h6>
-            </v-sheet>
-          </v-card>
-        </v-col>
-      </v-row>
+              <v-sheet class="mx-auto" width="85%">
+                <LoginForm> </LoginForm>
+                <v-divider class="my-4"></v-divider>
+                <h6 class="my-3 text-center">
+                  Don't have an account?
+                  <RouterLink to="/register" class="text-primary" style="text-decoration: none"
+                    >Sign up here<v-icon icon="mdi-chevron-right"></v-icon
+                  ></RouterLink>
+                </h6>
+              </v-sheet>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </template>
   </AppLayout>
 </template>
