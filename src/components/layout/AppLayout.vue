dd304245b57e722157e1a7174745577a62cdb188
kev <jankevin.dionaldo64@gmail.com>
2024-12-01 10:39:43 +0800

    feat: added supabase.js; backend initiation

--- src/components/layout/AppLayout.vue
+++ src/components/layout/AppLayout.vue
@@ -1,24 +1,13 @@
 <script setup>
-<<<<<<< HEAD
-import { ref } from 'vue'
-const theme = ref(localStorage.getItem('theme') ?? 'dark')
-=======
 import { ref, onMounted } from 'vue'
->>>>>>> 7d3009753df6cd6e9f6ba07f9491eba95b7b1f77
 
-const theme = ref(localStorage.getItem('theme') || 'dark') // Get the stored theme or default to 'dark'
+const theme = ref(localStorage.getItem('theme') || 'dark')
 
-// Function to toggle theme when switch is clicked
 function onClick() {
   theme.value = theme.value === 'dark' ? 'light' : 'dark'
-<<<<<<< HEAD
   localStorage.setItem('theme', theme.value)
-=======
-  localStorage.setItem('theme', theme.value) // Store the theme preference in localStorage
->>>>>>> 7d3009753df6cd6e9f6ba07f9491eba95b7b1f77
 }
 
-// On mount, apply the theme from localStorage if available
 onMounted(() => {
   document.body.setAttribute('data-theme', theme.value)
 })
@@ -29,10 +18,9 @@ onMounted(() => {
     <v-app :theme="theme">
       <v-app-bar class="px-7" :color="theme === 'dark' ? 'grey-darken-5' : 'grey-lighten-1'">
         <v-spacer></v-spacer>
-
-        <!-- Theme switch button -->
         <v-btn
-          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          :icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          variant="elevated"
           slim
           @click="onClick"
         ></v-btn>
