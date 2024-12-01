40db65ccb9efcc2931475652da0a6ff60915e573
kev <jankevin.dionaldo64@gmail.com>
2024-12-01 16:44:06 +0800

    feat: added dashboard and profile view; Config: register form and supabase

--- src/components/layout/AppLayout.vue
+++ src/components/layout/AppLayout.vue
@@ -1,8 +1,42 @@
 <script setup>
+import ProfileHeader from './ProfileHeader.vue'
+//import { useAuthUserStore } from '@/stores/authUser'
 import { ref, onMounted } from 'vue'
+import { useDisplay } from 'vuetify'
+import { isAuthenticated } from '@/utilities/supabase'
 
-const theme = ref(localStorage.getItem('theme') || 'dark')
+// For drawer
+//const props = defineProps(['isWithAppBarNavIcon'])
 
+//const emit = defineEmits(['isDrawerVisible', 'theme'])
+
+// Utilize pre-defined vue functions
+//const { xs, sm, mobile } = useDisplay()
+
+// Use Pinia Store
+//const authStore = useAuthUserStore()
+
+// Load Variables
+const isLoggedIn = ref(false)
+//const isMobileLogged = ref(false)
+//const isDesktop = ref(false)
+
+// Get Authentication status from supabase (needed for loading)
+const getLoggedStatus = async () => {
+  isLoggedIn.value = await isAuthenticated()
+}
+
+//isMobileLogged.value = mobile.value && isLoggedIn.value
+//isDesktop.value = !mobile.value && (isLoggedIn.value || !isLoggedIn.value)
+//}
+
+// Load Functions during component rendering
+//onMounted(() => {
+//getLoggedStatus()
+//})
+
+// For theme toggle
+const theme = ref(localStorage.getItem('theme') ?? 'light')
 function onClick() {
   theme.value = theme.value === 'dark' ? 'light' : 'dark'
   localStorage.setItem('theme', theme.value)
@@ -24,7 +58,10 @@ onMounted(() => {
           slim
           @click="onClick"
         ></v-btn>
+        <ProfileHeader v-if="isLoggedIn"></ProfileHeader>
       </v-app-bar>
+
+      <slot name="navigation"></slot>
 
       <v-main>
         <slot name="content"></slot>
---
+++ src/views/system/DashboardView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AppLayout from '@/components/layout/AppLayout.vue'
+import { ref } from 'vue'
+import SideNavigation from '@/components/layout/SideNavigation.vue'
+const isDrawerVisible = ref(true)
+</script>
+
+<template>
+  <AppLayout
+    :is-with-app-bar-nav-icon="true"
+    @is-drawer-visible="isDrawerVisible = !isDrawerVisible"
+  >
+    <template #navigation>
+      <SideNavigation :is-drawer-visible="isDrawerVisible"> </SideNavigation>
+    </template>
+    <template #content> </template>
+  </AppLayout>
+</template>
