f5bcdc31a966bf2596b7dfba3609350c51104e1b
JTubo <joshuatubo4@gmail.com>
2024-12-23 10:42:11 +0800

    feat: added cartStore.js && added: cart.js

--- src/stores/savedItems.js
+++ src/stores/savedItems.js
@@ -3,20 +3,20 @@ import { ref } from 'vue'
 
 export const useSavedItemsStore = defineStore('savedItems', {
   state: () => ({
-    savedItems: []
+    savedItems: [],
   }),
   actions: {
     saveItem(item) {
-      if (!this.savedItems.some(savedItem => savedItem.id === item.id)) {
+      if (!this.savedItems.some((savedItem) => savedItem.id === item.id)) {
         this.savedItems.push(item)
       }
     },
     removeItem(itemId) {
-      const index = this.savedItems.findIndex(item => item.id === itemId)
+      const index = this.savedItems.findIndex((item) => item.id === itemId)
       if (index !== -1) {
         this.savedItems.splice(index, 1)
       }
-    }
+    },
   },
-  persist: true
+  persist: true,
 })
