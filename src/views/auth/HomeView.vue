a9649b4043c096650d9085956de1781a50134330
kev <jankevin.dionaldo64@gmail.com>
2025-01-02 16:21:42 +0800

    feat: started building the homepage

--- src/views/auth/HomeView.vue
+++ src/views/auth/HomeView.vue
@@ -1,5 +1,50 @@
-<script setup></script>
+<script setup>
+import '@/assets/homepage.css'
+
+const cameras = ['images/camera3.png', 'images/camera2.jpg', 'images/camera4.jpg']
+
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId)
+  if (section) {
+    // Add a tiny offset to prevent layout jumps
+    const offset = section.getBoundingClientRect().top + window.scrollY - 1
+    window.scrollTo({
+      top: offset,
+      behavior: 'smooth',
+    })
+  }
+}
+</script>
 
 <template>
-  <v-responsive></v-responsive>
+  <v-responsive>
+    <v-container-fluid>
+      <v-row>
+        <v-col>
+          <v-layout>
+            <v-card
+              id="home"
+              :elevation="4"
+              color="grey-darken-4"
+              class="mx-auto pa-0 ma-0"
+              width="100%"
+              style="text-align: center; border-radius: 0"
+            >
+              <!--Carousel-->
+              <v-carousel :show-arrows="false" cycle hide-delimeters height="1000px">
+                <v-carousel-item
+                  v-for="(camera, i) in cameras"
+                  :key="i"
+                  :src="camera"
+                  draggable="false"
+                  cover
+                >
+                </v-carousel-item>
+              </v-carousel>
+            </v-card>
+          </v-layout>
+        </v-col>
+      </v-row>
+    </v-container-fluid>
+  </v-responsive>
 </template>
